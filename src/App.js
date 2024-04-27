6602e7308fb002ca82a63c283f84c219a5d9fee6
nikhil <nikhil.mathur2022@vitstudent.ac.in>
2024-04-27 15:11:39 +0530

    updated 2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       code
     };
     try{
-      const {data}=await axios.post("http://myapp-env-1.eba-z9zuveau.ap-south-1.elasticbeanstalk.com/run",payload)
+      const {data}=await axios.post("https://6xsnuinz0h.execute-api.ap-south-1.amazonaws.com/default/CodeEditor/run",payload)
       setOutput(data.output);
     }
     catch(err)
